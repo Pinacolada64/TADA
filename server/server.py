a46adf443abe56993df6e82ee3be30cd80e87ab9
cminter <corey.minter@gmail.com>
2021-11-25 14:07:31 -0800

    refactor room movement to be more object oriented

--- server/server.py
+++ server/server.py
@@ -44,6 +44,21 @@ class Room(object):
             if k in compass_txts:  exit_txts.append(compass_txts[k])
         return ", ".join(exit_txts)
 
+rooms = {}
+for info in roomsData:
+    room = Room(id=info[K.id], name=info[K.name], exits=info[K.exits])
+    rooms[room.id] = room
+
+server_lock = threading.Lock()
+room_players = {id: set() for id in rooms.keys()}
+
+def playersInRoom(room_id, exclude_id=None):
+    with server_lock:
+        players_in_room = room_players[room_id]
+    if exclude_id is not None:
+        players_in_room = players_in_room.difference(set([exclude_id]))
+    return players_in_room
+
 @dataclass
 class Player(object):
     id: str
@@ -53,19 +68,27 @@ class Player(object):
     health: int
     xp: int
 
-rooms = {}
-for info in roomsData:
-    room = Room(id=info[K.id], name=info[K.name], exits=info[K.exits])
-    rooms[room.id] = room
+    def connect(self):
+        with server_lock:
+            room_players[self.room].add(self.id)
 
+    def move(self, next_room):
+        prev_room = self.room
+        with server_lock:
+            room_players[self.room].remove(self.id)
+            self.room = next_room
+            room_players[self.room].add(self.id)
+
+    def disconnect(self):
+        with server_lock:
+            room_players[self.room].remove(self.id)
+
+        print(f"room {self.room} players:  {playersInRoom(self.room)}")
 players = {}
 for info in playersData:
     player = Player(id=info[K.id], name=info[K.name], money=info[K.money],
             room=info[K.room], health=info[K.health], xp=info[K.xp])
     players[player.id] = player
-
-server_lock = threading.Lock()
-room_players = {id: set() for id in rooms.keys()}
 
 class PlayerHandler(net_server.UserHandler):
 
@@ -81,8 +104,7 @@ class PlayerHandler(net_server.UserHandler):
         exitsTxt = room.exitsTxt()
         lines2 = list(lines)
         lines2.append(f"You are in {room_name} with exits to {exitsTxt}")
-        with server_lock:
-            other_player_ids = room_players[room.id].difference(set([self.player.id]))
+        other_player_ids = playersInRoom(room.id, self.player.id)
         if len(other_player_ids) > 0:
             other_players = ', '.join([players[id].name for id in other_player_ids])
             lines2.append(f"Other adventurers in the room:  {other_players}")
@@ -96,8 +118,7 @@ class PlayerHandler(net_server.UserHandler):
         changes = {K.room_name: rooms[self.player.room].name,
                 K.money: money, K.health: self.player.health,
                 K.xp: self.player.xp}
-        with server_lock:
-            room_players[self.player.room].add(self.player.id)
+        self.player.connect()
         return self.roomMsg(lines, changes)
 
     def processMessage(self, data):
@@ -108,13 +129,9 @@ class PlayerHandler(net_server.UserHandler):
             print(f"{self.player.id} {cmd=}")
             if cmd[0] in ['g', 'go']:
                 direction = cmd[1]
-                last_room_id = self.player.room
-                room = rooms[last_room_id]
+                room = rooms[self.player.room]
                 if direction in room.exits:
-                    self.player.room = next_room_id = room.exits[direction]
-                    with server_lock:
-                        room_players[last_room_id].remove(self.player.id)
-                        room_players[next_room_id].add(self.player.id)
+                    self.player.move(room.exits[direction])
                     room_name = rooms[self.player.room].name
                     return self.roomMsg(changes={'room_name': room_name})
                 else:
@@ -122,8 +139,7 @@ class PlayerHandler(net_server.UserHandler):
             if cmd[0] in ['look']:
                 return self.roomMsg()
             if cmd[0] in ['bye', 'logout']:
-                with server_lock:
-                    room_players[self.player.room].remove(self.player.id)
+                self.player.disconnect()
                 return Message(lines=["Bye for now."], mode=Mode.bye)
             if cmd[0] in ['help', 'cheatcode']:
                 return Message(lines=["Wouldn't that be nice."])
