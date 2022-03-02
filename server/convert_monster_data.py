c98c7f526d79cd3f3b5cfe790a28fa28c6b8dcca
Ryan Sherwood <sym.rsherwood@gmail.com>
2022-03-01 17:10:59 -0800

    Further along parsing monster data - parses flags - fix data[] indices - fix indentation - remove some old code

--- server/convert_monster_data.py
+++ server/convert_monster_data.py
@@ -118,98 +118,85 @@ def convert(txt_filename, monster_json_filename):
         then convert to lowercase with .lower():
         """
         # monster_name = re.compile("^M\.?:\d(.+(?:\|))")
-        while count < num_monsters:  # was 'while True' which... loops over the same item infinitely?
+        while count < num_monsters:
             data = read_stanza(file)
             monster_data = {}
             status = int(data[0])  # usually '1' for 'active'
+
             info = data[1]
-            # TODO: capture optional monster class number after 'M.':
+            # capture optional monster class number after 'M.':
             temp = info[2:1].isdigit()
-            start_name = 2  # starting position of name
             if temp:  # re.compile('^M\.(?:\d)', info)
-                monster_class = monster_classes.items(temp - 1)
-                start_name = 3
-            end_name = info.rfind("|")
-            if end_name == -1:  # not found
-                end_name = len(info)
-            name = info[start_name:end_name]
-            print(f'{name=}')
-            strength = data[3]
-            special_weapon = data[4]
-            to_hit = data[5]
+                monster_class = monster_classes.keys()
+                start_name = 3  # starting position of name
+            else:
+                monster_class = None
+                start_name = 2  # starting position of name
+            logging.info(f'{monster_class=}')
+
+            flag = info.rfind("|")
+            if flag == -1:  # not found
+                monster_flags = None
+                name = info[start_name:]
+                logging.info("(No flags)")
+            else:
+                # '|' in monster name. it has monster flags:
+                # trim name to before '|':
+                name = info[start_name:flag - 1]
+                # clear per-monster flag list:
+                flag_list = []
+                # FIXME: parse all the flags after the | symbol and add
+                #  their English keywords to flag_list
+                logging.info(f'Flags: {info[flag + 1:]}')
+                for k, v in monster_flags.items():
+                    if k in info[flag + 1:]:
+                        logging.info(f'with flag: {k=} {v=}')
+                        flag_list.append(v)
+                monster_data['flags'] = flag_list
+                logging.info(f'{monster_data["flags"]=}')
+            strength = int(data[2])
+            special_weapon = int(data[3])
+            to_hit = int(data[4])
             # toss "^" data block separator:
             _ = diskin(file)
-            print(f"""Raw input:\n
+            print(f"""Parsed input:\n
 {status=}
-{info=}
+{name=}
+{monster_class=}
+{flag_list=}
 {strength=}
 {special_weapon=}
 {to_hit=}
 """)
-            if status.startswith('#') is False and status != '':
-                count += 1
-                field = info.split(',')
-                if debug:
-                    for k in field:
-                        print(f"'{k}' ", end='')
-                    print()
-                monster_data['number'] = count
-                monster_data['type'] = monster_flags[field[0]]
 
-                # get monster name, strip trailing spaces:
-                temp = field[1].strip()
-                logging.info(f'{temp=}')
-
-                # if '|' in monster name, it has monster flags:
-                pos = temp.rfind('|')  # returns -1 if not found
-                if pos != -1:
-                    # trim name to before '|':
-                    name = temp[:pos]
-                    monster_data['name'] = name
-                    flags = temp[pos + 1:]  # flags after '|'
-                    # clear monster flag list:
-                    flag_list = []
-                    # FIXME: parse all the flags after the | symbol and add
-                    #  their English keywords to flag_list
-                    for k, v in monster_flags.items():
-                        if k in flags:
-                            logging.info(f'with flag: {k=} {v=}')
-                            flag_list.append(v)
-                    monster_data['flags'] = flag_list
-                    logging.info(f'{monster_data["flags"]=}')
+            # TODO: maybe descriptions later
+            """
+            descLines = []
+            moreLines = True
+            while moreLines:
+                line = diskin(file)
+                if line != "^":
+                    descLines.append(line)
                 else:
-                    monster_data['name'] = field[1].rstrip(' ')
-                    monster_data['flags'] = None
-                    logging.info("(No flags)")
+                    moreLines = False
+            itemData['desc'] = " ".join(descLines)
+            """
 
-                # TODO: maybe descriptions later
-                """
-                descLines = []
-                moreLines = True
-                while moreLines:
-                    line = diskin(file)
-                    if line != "^":
-                        descLines.append(line)
-                    else:
-                        moreLines = False
-                itemData['desc'] = " ".join(descLines)
-                """
-
-                if debug:
-                    type = monster_data['type']
-                    name = monster_data['name']
-                    try:
-                        flags = monster_data['flags']
-                    except ValueError:
-                        flags = '(None)'
-                    print(f'{count=} {type=} {name=} {flags=}')
-                item = Items(**monster_data)
-                logging.info(f"*** processed monster '{monster_data['name']}'")
-                monster_list.append(item)
-                if debug:
-                    if count % 20 == 0:
-                        _ = input("Hit Return: ")
-                        # logging.info(f'{count=} {len(monster_list)=}')
+            if debug:
+                # type = monster_data['type']
+                name = monster_data['name']
+                try:
+                    flags = monster_data['flags']
+                except ValueError:
+                    flags = '(None)'
+                print(f'{count=} {type=} {name=} {flags=}')
+            item = Items(**monster_data)
+            logging.info(f"*** processed monster '{monster_data['name']}'")
+            monster_list.append(item)
+            if debug:
+                if count % 20 == 0:
+                    _ = input("Hit Return: ")
+                    # logging.info(f'{count=} {len(monster_list)=}')
 
         if write is True:
             with open(monster_json_filename, 'w') as monster_json:
