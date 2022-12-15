8c690ef7070999e4045d1f7f72d9858c89b42d37
Ryan Sherwood <sym.rsherwood@gmail.com>
2022-12-15 11:23:24 -0800

    Changed bar map Added opening message, input hints Added coordinates and names of places to go

--- server/bar.py
+++ server/bar.py
@@ -1,12 +1,19 @@
-bar = ["********************",
-       "*                  *",
-       "*                  *",
-       "*******      *******",
-       "*     *      *     *",
-       "*                  *",
-       "*     *      *     *",
-       "********************"
+bar = ["+----+ +----+",
+       "|o[]     []o|",
+       "|           |",
+       "|--+     []o|",
+       "|oo|     []o|",
+       "+-----------+"
        ]
+
+# y, x, name
+locations = [(0, 6, "Exit"),
+             (1, 4, "The Blue Djinn"),
+             (1, 8, "Vinney the Loan Shark"),
+             (2, 1, "Skip's Eats"),
+             (3, 8, "Fat Olaf's Slave Trade"),
+             (4, 4, "Bar None"),
+             (4, 8, "Madame Zelda's")]
 
 # pos is zero-based from the top down, left-to-right
 pos_y = 3  # row
@@ -31,6 +38,9 @@ def horizontal_ruler():
     print((digits * 10)[:ruler_length])
 
 
+print("You stand in the doorway of a smoky bar. A faded sign reads:")
+print('"WALL BAR AND GRILL."\n')
+
 while True:
     count = 0
     horizontal_ruler()
@@ -42,10 +52,25 @@ while True:
             print(line)
         count += 1
 
+    go_here = False
+    for place in locations:
+        # sorted by rows
+        if pos_y == place[0] and pos_x == place[1]:
+            print(place[2], "\n")
+            go_here = True
+
     print(f'{pos_x=}, {pos_y=}')
+
+    extra = ", G)o here" if go_here else ''
+
+    print(f"N)orth, E)ast, S)outh, W)est{extra}")
+    print()
     direction = input("Which way? ").lower()
     print()
     valid_move = False
+
+    if go_here and direction == "g":
+        print("Chosen.")
 
     if direction == 'n':
         # look up for obstacle:
