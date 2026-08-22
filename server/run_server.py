0a39434205be70cb0812c9b0d39428215ae8a21d
Ryan Sherwood <sym.rsherwood@gmail.com>
2026-08-21 17:27:04 -0700

    Default server bind host to 0.0.0.0 instead of localhost
    Lets the server accept connections from other machines out of the box,
    not just loopback.

--- server/run_server.py
+++ server/run_server.py
@@ -29,7 +29,7 @@ logging.basicConfig(
 logging.getLogger().handlers[0].addFilter(_PlayerFilter())
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--host', default='127.0.0.1')
+parser.add_argument('--host', default='0.0.0.0')
 parser.add_argument('--port', type=int, default=34083)
 args = parser.parse_args()
 
